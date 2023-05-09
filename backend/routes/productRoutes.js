3b3c2b0c427783f500181ab0bf6af790e13c4686
Jared Brown <jaredbrown1@hotmail.com>
2023-05-08 19:25:15 -0700

    error handling

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -13,6 +13,9 @@ router.get('/:id', asyncHandler(async (req, res) => {
 
     if (product) {
         res.json(product);
+    } else {
+        res.status(404);
+        throw new Error('Resource not found')
     }
 
     res.status(404).json({ message: 'Product not found' });
