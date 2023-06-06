56fc0ed8d6622e4b808b81db7d72f00c26498680
Jared Brown <jaredbrown1@hotmail.com>
2023-06-05 22:12:57 -0700

    user controller layout finished

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import {notFound, errorHandler} from './middleware/errorMiddleware.js';
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 6000;
 
@@ -11,14 +12,14 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
+app.get("/", (req, res) => {
+	res.send("API is running...");
 });
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
-app.use(errorHandler)
-
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
