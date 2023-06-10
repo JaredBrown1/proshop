f1575cbdae2cdebd57fac0d9baea8f931233b869
Jared Brown <jaredbrown1@hotmail.com>
2023-06-09 22:39:58 -0700

    protected routes and admin routes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,21 +1,23 @@
 import jwt from "jsonwebtoken";
-import asyncHandler from "./asyncHandler";
-import User from "../models/userModel";
+import asyncHandler from "./asyncHandler.js";
+import User from "../models/userModel.js";
 
-// Protect Routes
+// User must be authenticated
 const protect = asyncHandler(async (req, res, next) => {
 	let token;
 
-	// Read the JWT from the cookie
+	// Read JWT from the 'jwt' cookie
 	token = req.cookies.jwt;
 
 	if (token) {
 		try {
 			const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
 			req.user = await User.findById(decoded.userId).select("-password");
+
 			next();
 		} catch (error) {
-			console.log(error);
+			console.error(error);
 			res.status(401);
 			throw new Error("Not authorized, token failed");
 		}
@@ -25,7 +27,7 @@ const protect = asyncHandler(async (req, res, next) => {
 	}
 });
 
-// Admin middlware
+// User must be an admin
 const admin = (req, res, next) => {
 	if (req.user && req.user.isAdmin) {
 		next();
